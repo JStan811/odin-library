c037bc384c776db1c652a3f9d59ccb5ec14d0a30
Jake Standley <jakestandley95@gmail.com>
2022-01-19 09:40:45 -0800

    Add defer to js script tag

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,14 @@
 let myLibrary = [];
 
-function Book(title, author, pages, read) {
+function Book(title, author, pages, readStatus) {
   this.title = title;
   this.author = author;
   this.pages = pages;
-  this.read = read;
+  this.readStatus = readStatus;
 }
 
 Book.prototype.info = function () {
-  if(this.read === true) {
+  if(this.readStatus === true) {
     return `${this.title} by ${this.author}, ${this.pages} pages, read.`
   }
   else {
@@ -20,16 +20,80 @@ function addBookToLibrary() {
   let title = prompt("What is the book's title?");
   let author = prompt("Who is the book's author?");
   let pages = prompt("How many pages does the book have?");
-  let read_answer = prompt("Have you read the book? Enter y for yes. Any other response will be taken as no.");
-  let read = false;
+  let readStatusResponse = prompt("Have you read the book? Enter y for yes. Any other response will be taken as no.");
+  let readStatus = false;
 
-  if(read_answer === 'y') {
-    read = true;
+  if(readStatusResponse === 'y') {
+    readStatus = true;
   }
 
-  myLibrary.push(new Book(title, author, pages, read))
+  myLibrary.push(new Book(title, author, pages, readStatus))
 }
 
-// addBookToLibrary();
+function createBookCardElement() {
+  const bookCard = document.createElement('article');
+  bookCard.classList.add('card');
+  return bookCard;
+}
 
-// console.log(myLibrary);
+function createBookCardImgElement() {
+  const bookCardImg = document.createElement('img');
+  bookCardImg.src = 'img/default-cover.jpg';
+  bookCardImg.alt = 'Default generic book cover'
+  return bookCardImg;
+}
+
+function createBookCardTextElement() {
+  const bookCardText = document.createElement('section');
+  bookCardText.classList.add('card-text');
+  return bookCardText;
+}
+
+function createBookTitleElement() {
+  const bookTitle = document.createElement('h2');
+  bookTitle.textContent = 'Book Title';
+  return bookTitle;
+}
+
+function createBookAuthorElement() {
+  const bookAuthor = document.createElement('p');
+  bookAuthor.textContent = 'Book Author';
+  return bookAuthor;
+}
+
+function createBookPagesElement() {
+  const bookPages = document.createElement('p');
+  bookPages.textContent = 'Book Pages';
+  return bookPages;
+}
+
+function createBookReadStatusElement () {
+  const bookReadStatus = document.createElement('p');
+  bookReadStatus.textContent = 'Book Read Status';
+  return bookReadStatus;
+}
+
+function addContentsToBookCard(bookCard) {
+  bookCard.appendChild(createBookCardImgElement());
+  const bookCardText = createBookCardTextElement();
+  addContentsToBookCardText(bookCardText);
+  bookCard.appendChild(bookCardText);
+}
+
+function addContentsToBookCardText(bookCardText) {
+  bookCardText.appendChild(createBookTitleElement());
+  bookCardText.appendChild(createBookAuthorElement());
+  bookCardText.appendChild(createBookPagesElement());
+  bookCardText.appendChild(createBookReadStatusElement());
+}
+
+function displayBookCards() {
+  const cardContainer = document.querySelector('.card-container');
+
+  const bookCard = createBookCardElement();
+  addContentsToBookCard(bookCard);
+  cardContainer.appendChild(bookCard);
+}
+
+// Functions called upon page load
+displayBookCards();
