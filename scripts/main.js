32b63d39d308a2c1098b3b670e0a789e508a203b
Jake Standley <jakestandley95@gmail.com>
2022-01-18 16:35:33 -0800

    Change info function to prototype
    TIL its usually better to define a function via prototype than in the constructor function so I update my code.

--- scripts/main.js
+++ scripts/main.js
@@ -5,14 +5,14 @@ function Book(title, author, pages, read) {
   this.author = author;
   this.pages = pages;
   this.read = read;
+}
 
-  this.info = function() {
-    if(this.read === true) {
-      return `${title} by ${author}, ${pages} pages, read.`
-    }
-    else {
-      return `${title} by ${author}, ${pages} pages, not read yet.`
-    }
+Book.prototype.info = function () {
+  if(this.read === true) {
+    return `${this.title} by ${this.author}, ${this.pages} pages, read.`
+  }
+  else {
+    return `${this.title} by ${this.author}, ${this.pages} pages, not read yet.`
   }
 }
 
@@ -20,7 +20,7 @@ function addBookToLibrary() {
   let title = prompt("What is the book's title?");
   let author = prompt("Who is the book's author?");
   let pages = prompt("How many pages does the book have?");
-  let read_answer = prompt("Have you read the book? Enter y or n");
+  let read_answer = prompt("Have you read the book? Enter y for yes. Any other response will be taken as no.");
   let read = false;
 
   if(read_answer === 'y') {
