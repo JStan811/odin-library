bef5b580eeac5562aa9b621d2e2ed34154cf00a4
Jake Standley <jakestandley95@gmail.com>
2022-01-19 10:24:42 -0800

    Change script to use a Library Object
    Before this, I just had each function sitting at the base (global?) level, unattached to any object. Since this project follows the Objects and Object Constructors lesson in the curriculum, I decided to instead create a new Library object and put all of the functions into Library.prototype. Now my script creates a new Library object and then runs its own functions to produce the card objects. One thing I noticed during this work is that if you put the main scripts (ie what actually is running on page load) at the top of the document, it fails because the functions haven't been defined yet. I would have though the script goes through and creates all objects and functions before running anything, but guess not?

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,89 @@
-let myLibrary = [];
+// Library object
+
+function Library() {
+  this.books = [];
+}
+
+// function addBookToLibrary() {
+//   let title = prompt("What is the book's title?");
+//   let author = prompt("Who is the book's author?");
+//   let pages = prompt("How many pages does the book have?");
+//   let readStatusResponse = prompt("Have you read the book? Enter y for yes. Any other response will be taken as no.");
+//   let readStatus = false;
+
+//   if(readStatusResponse === 'y') {
+//     readStatus = true;
+//   }
+
+//   myLibrary.push(new Book(title, author, pages, readStatus))
+// }
+
+Library.prototype.createBookCardElement = function() {
+  const bookCard = document.createElement('article');
+  bookCard.classList.add('card');
+  return bookCard;
+}
+
+Library.prototype.createBookCardImgElement = function() {
+  const bookCardImg = document.createElement('img');
+  bookCardImg.src = 'img/default-cover.jpg';
+  bookCardImg.alt = 'Default generic book cover'
+  return bookCardImg;
+}
+
+Library.prototype.createBookCardTextElement = function() {
+  const bookCardText = document.createElement('section');
+  bookCardText.classList.add('card-text');
+  return bookCardText;
+}
+
+Library.prototype.createBookTitleElement = function() {
+  const bookTitle = document.createElement('h2');
+  bookTitle.textContent = 'Book Title';
+  return bookTitle;
+}
+
+Library.prototype.createBookAuthorElement = function() {
+  const bookAuthor = document.createElement('p');
+  bookAuthor.textContent = 'Book Author';
+  return bookAuthor;
+}
+
+Library.prototype.createBookPagesElement = function() {
+  const bookPages = document.createElement('p');
+  bookPages.textContent = 'Book Pages';
+  return bookPages;
+}
+
+Library.prototype.createBookReadStatusElement = function() {
+  const bookReadStatus = document.createElement('p');
+  bookReadStatus.textContent = 'Book Read Status';
+  return bookReadStatus;
+}
+
+Library.prototype.addContentsToBookCard = function(bookCard) {
+  bookCard.appendChild(this.createBookCardImgElement());
+  const bookCardText = this.createBookCardTextElement();
+  this.addContentsToBookCardText(bookCardText);
+  bookCard.appendChild(bookCardText);
+}
+
+Library.prototype.addContentsToBookCardText = function(bookCardText) {
+  bookCardText.appendChild(this.createBookTitleElement());
+  bookCardText.appendChild(this.createBookAuthorElement());
+  bookCardText.appendChild(this.createBookPagesElement());
+  bookCardText.appendChild(this.createBookReadStatusElement());
+}
+
+Library.prototype.displayBookCards = function() {
+  const cardContainer = document.querySelector('.card-container');
+
+  const bookCard = this.createBookCardElement();
+  this.addContentsToBookCard(bookCard);
+  cardContainer.appendChild(bookCard);
+}
+
+// Book object
 
 function Book(title, author, pages, readStatus) {
   this.title = title;
@@ -9,91 +94,16 @@ function Book(title, author, pages, readStatus) {
 
 Book.prototype.info = function () {
   if(this.readStatus === true) {
-    return `${this.title} by ${this.author}, ${this.pages} pages, read.`
+    return `${this.title} by ${this.author}, ${this.pages} pages, read.`;
   }
   else {
-    return `${this.title} by ${this.author}, ${this.pages} pages, not read yet.`
+    return `${this.title} by ${this.author}, ${this.pages} pages, not read yet
+        `;
   }
 }
 
-function addBookToLibrary() {
-  let title = prompt("What is the book's title?");
-  let author = prompt("Who is the book's author?");
-  let pages = prompt("How many pages does the book have?");
-  let readStatusResponse = prompt("Have you read the book? Enter y for yes. Any other response will be taken as no.");
-  let readStatus = false;
+// Main
+const library = new Library();
 
-  if(readStatusResponse === 'y') {
-    readStatus = true;
-  }
+library.displayBookCards();
 
-  myLibrary.push(new Book(title, author, pages, readStatus))
-}
-
-function createBookCardElement() {
-  const bookCard = document.createElement('article');
-  bookCard.classList.add('card');
-  return bookCard;
-}
-
-function createBookCardImgElement() {
-  const bookCardImg = document.createElement('img');
-  bookCardImg.src = 'img/default-cover.jpg';
-  bookCardImg.alt = 'Default generic book cover'
-  return bookCardImg;
-}
-
-function createBookCardTextElement() {
-  const bookCardText = document.createElement('section');
-  bookCardText.classList.add('card-text');
-  return bookCardText;
-}
-
-function createBookTitleElement() {
-  const bookTitle = document.createElement('h2');
-  bookTitle.textContent = 'Book Title';
-  return bookTitle;
-}
-
-function createBookAuthorElement() {
-  const bookAuthor = document.createElement('p');
-  bookAuthor.textContent = 'Book Author';
-  return bookAuthor;
-}
-
-function createBookPagesElement() {
-  const bookPages = document.createElement('p');
-  bookPages.textContent = 'Book Pages';
-  return bookPages;
-}
-
-function createBookReadStatusElement () {
-  const bookReadStatus = document.createElement('p');
-  bookReadStatus.textContent = 'Book Read Status';
-  return bookReadStatus;
-}
-
-function addContentsToBookCard(bookCard) {
-  bookCard.appendChild(createBookCardImgElement());
-  const bookCardText = createBookCardTextElement();
-  addContentsToBookCardText(bookCardText);
-  bookCard.appendChild(bookCardText);
-}
-
-function addContentsToBookCardText(bookCardText) {
-  bookCardText.appendChild(createBookTitleElement());
-  bookCardText.appendChild(createBookAuthorElement());
-  bookCardText.appendChild(createBookPagesElement());
-  bookCardText.appendChild(createBookReadStatusElement());
-}
-
-function displayBookCards() {
-  const cardContainer = document.querySelector('.card-container');
-
-  const bookCard = createBookCardElement();
-  addContentsToBookCard(bookCard);
-  cardContainer.appendChild(bookCard);
-}
-
-// Functions called upon page load
-displayBookCards();
